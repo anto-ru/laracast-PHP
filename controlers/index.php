0367a58ca234ad5f48bed7a74467c036631d37de
Antonio-WebLab <antonio.web.lab@gmail.com>
2025-04-17 16:34:25 +0100

    first commit

---
+++ controlers/index.php
@@ -0,0 +1,4 @@
+<?php
+$heading = "INDEX ";
+
+require('views/index.view.php');
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require "index.view.php";
+
+// il metodo parse_url analizza e divide la URL e restituisce un array associativo, da cui possiamo estrarre il path
+/* 
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$route = [
+    '/laracast-PHP/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/services' => 'controllers/services.php',
+    
+];
+
+if (array_key_exists($uri, $route)) {
+    require $route[$uri];
+} else {
+    http_response_code(404);
+    require('views/404.php');
+}
+*/
