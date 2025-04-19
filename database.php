91c4def442b319b8a73fab81a5cf3eac44d2022b
Antonio-WebLab <antonio.web.lab@gmail.com>
2025-04-19 05:07:03 +0100

    UPDATE database.php

--- database.php
+++ database.php
@@ -15,7 +15,7 @@ try {
 }
 
 
-$dsn = "mysql:host=localhost;port=3306;dbname=local;charset=utf8mb4";
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
 $username = "root";
 $password = "root";
 
@@ -27,25 +27,46 @@ try {
     echo "Errore di connessione: " . $e->getMessage();
 }
 
+/*
 $statement = $pdo->prepare 
 ("SELECT USER();           
 SELECT DATABASE();       
 SHOW VARIABLES LIKE 'port';
 SHOW TABLES;");
-
-/*
-$statement = $pdo->prepare 
-("select * from articoli");
 */
 
-$statement->execute();
+// $statement = $pdo->prepare ("create database myapp;");
+// $statement = $pdo->prepare ("use myapp;");
 
-$articoli = $statement->fetchAll(PDO::FETCH_ASSOC);
+// $statement = $pdo->prepare (" CREATE TABLE articoli (id INT AUTO_INCREMENT PRIMARY KEY, titolo VARCHAR(250) NOT NULL );");
 
-/*
+// $statement = $pdo->prepare ("select * from articoli");
+ $statement = $pdo->prepare ("SELECT DATABASE();");
+ $statement->execute();
+ $nome_database = $statement->fetchAll(PDO::FETCH_ASSOC);
+ echo "<pre>";
+    print_r($nome_database);
+    echo "</pre>";
+// $statement = $pdo->prepare ("SHOW TABLES;");
+
+ $statement2 = $pdo->prepare ("select * from articoli");
+// $statement = $pdo->prepare (" CREATE TABLE articoli (id INT AUTO_INCREMENT PRIMARY KEY, titolo VARCHAR(250) NOT NULL );");
+
+// $statement = $pdo->prepare ("INSERT INTO articoli (titolo)VALUES ('il mio primo articolo')");
+// $statement = $pdo->prepare ("INSERT INTO articoli (titolo)VALUES ('il mio secondo articolo')");
+
+
+
+
+$statement2->execute();
+
+// $articoli = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$articoli = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
 foreach ($articoli as $articolo) {
     echo "<li>" . $articolo['titolo'] . "</li>";
 }
-*/
 
-dd($articoli);
+
+// dd($articoli);
